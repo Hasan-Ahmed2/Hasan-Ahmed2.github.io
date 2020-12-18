2df421620eefed3a80b7f25555455f51aea1ebde
Hasan-Ahmed2 <75853030+Hasan-Ahmed2@users.noreply.github.com>
2020-12-18 11:51:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+var counter = 0;
 
+
+$(".img1").click(function () {
+      counter = counter + 1;
+    $(".img1Text").text("Link clicked " + counter + " times");
+    
+});
+
+var counter2 = 0;
+
+$(".img2").click(function () {
+      counter2 = counter2 + 1;
+    $(".img2Text").text("Link clicked " + counter2 + " times");
+    
+});
+
+var counter3 = 0;
+
+$(".img3").click(function () {
+      counter3 = counter3 + 1;
+    $(".img3Text").text("Link clicked " + counter3 + " times");
+    
+});
+
+var counter4 = 0;
+
+$(".img4").click(function () {
+      counter4 = counter4 + 1;
+    $(".img4Text").text("Link clicked " + counter4 + " times");
+    
+});
